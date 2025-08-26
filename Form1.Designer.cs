dccdbbaf6771074f2eb5167b5c0234d2f794bf3f
hayeong120 <jbg01200249@gmail.com>
2025-08-26 09:36:49 +0900

    #1 사용자 입력 폼 구현

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,46 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbBirthtime = new System.Windows.Forms.GroupBox();
+            this.btnShowResult = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbBirthday = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbBirthtime.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // tbBirthtime
             // 
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(385, 426);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            this.tbBirthtime.Controls.Add(this.button1);
+            this.tbBirthtime.Controls.Add(this.textBox1);
+            this.tbBirthtime.Controls.Add(this.tbBirthday);
+            this.tbBirthtime.Controls.Add(this.label2);
+            this.tbBirthtime.Controls.Add(this.label1);
+            this.tbBirthtime.Location = new System.Drawing.Point(12, 12);
+            this.tbBirthtime.Name = "tbBirthtime";
+            this.tbBirthtime.Size = new System.Drawing.Size(385, 426);
+            this.tbBirthtime.TabIndex = 0;
+            this.tbBirthtime.TabStop = false;
+            this.tbBirthtime.Text = "사용자 입력";
+            this.tbBirthtime.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // groupBox2
+            // btnShowResult
             // 
-            this.groupBox2.Location = new System.Drawing.Point(403, 12);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(385, 426);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
+            this.btnShowResult.Location = new System.Drawing.Point(403, 12);
+            this.btnShowResult.Name = "btnShowResult";
+            this.btnShowResult.Size = new System.Drawing.Size(385, 426);
+            this.btnShowResult.TabIndex = 1;
+            this.btnShowResult.TabStop = false;
+            this.btnShowResult.Text = "사주 결과";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "생년월일을 입력하세요(예: 20070101)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(245, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "태어난 시각을 입력해주세요(예: 08시 -> 08)";
+            // 
+            // tbBirthday
+            // 
+            this.tbBirthday.Location = new System.Drawing.Point(64, 120);
+            this.tbBirthday.Name = "tbBirthday";
+            this.tbBirthday.Size = new System.Drawing.Size(245, 21);
+            this.tbBirthday.TabIndex = 2;
+            this.tbBirthday.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 179);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "결과 보러가기";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnShowResult);
+            this.Controls.Add(this.tbBirthtime);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "포츈텔러";
+            this.tbBirthtime.ResumeLayout(false);
+            this.tbBirthtime.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox tbBirthtime;
+        private System.Windows.Forms.GroupBox btnShowResult;
+        private System.Windows.Forms.TextBox tbBirthday;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,10 @@ namespace Forturn_Teller
         {
 
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
