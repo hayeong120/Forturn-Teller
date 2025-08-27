98f4b224cb0ebe25c2e7f9814758b080525275ad
hayeong120 <jbg01200249@gmail.com>
2025-08-27 10:03:52 +0900

    #8 내역 저장하는 SaveHistory 메서드 구현

--- Form1.cs
+++ Form1.cs
@@ -29,12 +29,12 @@ namespace Forturn_Teller
             }
             catch (FileNotFoundException ex)
             {
-                MessageBox.Show($"파일이 없어요.\n{ex.Message}", "파일이 없는 오류!",
+                MessageBox.Show($"파일이 없습니다.\n{ex.Message}", "파일이 없는 오류!",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             catch (UnauthorizedAccessException ex)
             {
-                MessageBox.Show($"권한이 없어요..\n{ex.Message}", "파일 권한 오류!",
+                MessageBox.Show($"파일에 접근 권한이 없습니다.\n{ex.Message}", "파일 권한 오류!",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             catch (Exception ex)
@@ -94,13 +94,30 @@ namespace Forturn_Teller
         private void button1_Click(object sender, EventArgs e)
         {
             string birthday = tbBirthday.Text;
-            string birthtime = GroupBox.Text;
+            string birthtime = tbBirthtime.Text;
             string result = GetFortune();
             string saju = result.Split('|')[0];
             string message = result.Split('|')[1];
             tbResult.Text = birthday + " " + birthtime + Environment.NewLine
-                + saju + Environment.NewLine + result;
+                + saju + Environment.NewLine + message;
+            SaveHistory($"{birthday} {birthtime}|{result}");
         }
 
+        private void SaveHistory(string history)
+        {
+            try
+            {
+                string filename = "history.csv";
+                File.AppendAllText(filename, history + Environment.NewLine);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"파일에 접근 권한이 없습니다.\n{ex.Message}", "파일 권한 오류!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"알 수 없는 오류가 발생했습니다.\n{ex.Message}", "알 수 없는 오류!");
+            }   
+        }
     }
 }
